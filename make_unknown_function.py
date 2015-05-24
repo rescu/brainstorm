d56092f5bc26c6d44c2c197fe28ed3769ed08336
Will Barnes <wtb2@rice.edu>
2015-05-23 21:21:17 -0500

    added bisection method function and application to unknown function section

--- make_unknown_function.py
+++ make_unknown_function.py
@@ -8,12 +8,13 @@ def sine_integral_func(x):
     return tmp2
 
 #construct sine integral over range 0<x<2*pi
-x = np.arange(0.0,2.0*np.pi,0.01)
+x = np.arange(0.0,2.0*np.pi,0.001)
 y = []
 [y.append(sine_integral_func(i)) for i in x]
 
 #save the x and y vectors to a file
 save_list = [x,y]
-np.savetxt('example_data/root_finding_unknown_function.txt',save_list)
-
-
+path2file='example_data/root_finding_unknown_function.txt'
+np.savetxt(path2file,save_list)
+print "x and y data for unknown function saved in ",path2file
+print "Name of unknown function is sine_integral_func"
